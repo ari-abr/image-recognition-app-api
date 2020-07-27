5b10a2ce17084454e3ef2cf83e35e2fcd8dfcee3
Masha A <mabramovaa01@gmail.com>
2020-07-27 15:52:53 -0400

    connectionString

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   },
 });
