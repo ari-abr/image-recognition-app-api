4aa2d9f14c7a3fa5b3f62fa862cb6d05c8f1d1fd
Masha A <mabramovaa01@gmail.com>
2020-07-27 14:55:07 -0400

    My first commit

---
+++ server.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: '7513',
+    database: 'face-rec-db',
+  },
+});
+app.get('/', (req, res) => {
+  res.send(db.users);
+});
+
+db.select('*')
+  .from('users')
+  .then((data) => {
+    console.log(data);
+    console.log('data has loaded');
+  });
+
+app.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    console.log(email, password);
+    return res.status(400).json('incorrect form submission');
+  }
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json('unable to get user'));
+      } else {
+        res.status(400).json('wrong credentials');
+      }
+    })
+    .catch((err) => res.status(400).json('wrong credentials'));
+  /*console.log(req.body.email)
+      console.log(req.body.password)
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+      console.log(data[0].hash)
+      console.log(isValid)
+      if(isValid){
+        console.log('user is good!')
+        return db.select('*').from('users')
+        .where('email', '=', req.body.email)
+        .then(user => { res.json(user[0])})
+        .catch(err => res.status(400).jason(err))
+       /*try{
+          const user = db.select('*').from('users')
+            .where('email', '=', req.body.email);
+          res.json(user[0]);
+        }
+        catch (err) {
+          console.log(err)
+          return res.status(400).json("oops");
+        }
+     } else {
+      console.log("oh no")
+        res.status(400).json('wrong info')
+      }
+    })
+    .catch(err => res.status(400).json(err))*/
+});
+
+///register, register.hadleRegister)
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('wrong form submission');
+  }
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json('unable to register'));
+});
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  db.select('*')
+    .from('users')
+    .where({ id: id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('not found');
+      }
+    })
+    .catch((err) => res.status(400).json('error getting the user'));
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json('no entry count'));
+});
+
+app.listen(3000, () => {
+  console.log('running on port 3000');
+});
