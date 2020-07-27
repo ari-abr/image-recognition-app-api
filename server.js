51bf26ff13f568f123e4b72a31cbbab479035267
Masha A <mabramovaa01@gmail.com>
2020-07-27 18:52:54 -0400

    psql thing

--- server.js
+++ server.js
@@ -52,35 +52,6 @@ app.post('/signin', (req, res) => {
       }
     })
     .catch((err) => res.status(400).json('wrong credentials'));
-  /*console.log(req.body.email)
-      console.log(req.body.password)
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
-      console.log(data[0].hash)
-      console.log(isValid)
-      if(isValid){
-        console.log('user is good!')
-        return db.select('*').from('users')
-        .where('email', '=', req.body.email)
-        .then(user => { res.json(user[0])})
-        .catch(err => res.status(400).jason(err))
-       /*try{
-          const user = db.select('*').from('users')
-            .where('email', '=', req.body.email);
-          res.json(user[0]);
-        }
-        catch (err) {
-          console.log(err)
-          return res.status(400).json("oops");
-        }
-     } else {
-      console.log("oh no")
-        res.status(400).json('wrong info')
-      }
-    })
-    .catch(err => res.status(400).json(err))*/
 });
 
 ///register, register.hadleRegister)
